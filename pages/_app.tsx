23b00507810e8d2475bde9d65f30ee189384401a
Mirka Juliet <72041260+MirkaJuliet34@users.noreply.github.com>
2022-01-16 23:27:34 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 
 import { ThemeProvider } from "next-themes";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ThemeProvider attribute="class">
       <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
